81860c15a13ef63c7281d86eeae2abc9c8faaa31
cookiedragon234 <cookie.dragon234@gmail.com>
2020-01-31 22:58:06 +0000

    Work on bootstrap method (string decryption)

--- src/main/kotlin/cookiedragon/obfuscator/processors/indirection/DynamicCallObfuscation.kt
+++ src/main/kotlin/cookiedragon/obfuscator/processors/indirection/DynamicCallObfuscation.kt
@@ -5,13 +5,13 @@ import cookiedragon.obfuscator.IClassProcessor
 import cookiedragon.obfuscator.classpath.ClassPath
 import cookiedragon.obfuscator.configuration.ConfigurationManager
 import cookiedragon.obfuscator.kotlin.wrap
+import cookiedragon.obfuscator.kotlin.xor
 import cookiedragon.obfuscator.processors.renaming.impl.ClassRenamer
+import cookiedragon.obfuscator.utils.ldcInt
 import org.objectweb.asm.Handle
+import org.objectweb.asm.Label
 import org.objectweb.asm.Opcodes.*
-import org.objectweb.asm.tree.ClassNode
-import org.objectweb.asm.tree.InvokeDynamicInsnNode
-import org.objectweb.asm.tree.MethodInsnNode
-import org.objectweb.asm.tree.MethodNode
+import org.objectweb.asm.tree.*
 
 /**
  * @author cookiedragon234 22/Jan/2020
@@ -63,21 +63,135 @@ object DynamicCallObfuscation: IClassProcessor {
 				null,
 				null
 			).apply {
+				generateBootstrapMethod(this)
 				decryptNode.methods.add(this)
 			}
 			
 			val handler = Handle(H_INVOKESTATIC, decryptNode.name, bootStrapMethod.name, bootStrapMethod.desc, false)
 			
 			for (methodCall in methodCalls) {
+				val desc = if (methodCall.insnNode.opcode == INVOKESTATIC) {
+					methodCall.insnNode.desc
+				} else {
+					methodCall.insnNode.desc.replaceFirst("(", "(Ljava/lang/Object;")
+				}
+				
 				methodCall.methodNode.instructions.set(
 					methodCall.insnNode,
-					InvokeDynamicInsnNode(methodCall.owner, "", handler) // TODO:
+					InvokeDynamicInsnNode(
+						encryptName(methodCall.classNode.name, methodCall.methodNode.name, methodCall.insnNode),
+						desc,
+						handler
+					)
 				)
 			}
 		}
 	}
 	
-	fun encryptName(className: String, methodName: String, 
+	private fun encryptName(className: String, methodName: String, methodNode: MethodInsnNode): String {
+		val classHash = className.replace('/', '.').hashCode()
+		val methodHash = methodName.replace('/', '.').hashCode()
+		
+		var originalStr = "${methodNode.owner}.${methodNode.name}.${methodNode.desc}"
+		if (methodNode.opcode == INVOKESTATIC) {
+			originalStr += "s"
+		} else {
+			originalStr += "v"
+		}
+		val original = originalStr.toCharArray()
+		val new = CharArray(original.size)
+		
+		for (i in 0 until original.size) {
+			val char = original[i]
+			new[i] = when (i % 5) {
+				0 -> char xor 2
+				1 -> char xor classHash
+				2 -> char xor methodHash
+				3 -> char xor (classHash + methodHash)
+				4 -> char xor i
+				else -> throw IllegalStateException("Illegal ${i % 6}")
+			}
+		}
+		return String(new)
+	}
+	
+	private fun generateBootstrapMethod(methodNode: MethodNode) {
+		methodNode.instructions.apply {
+			val loopStart = LabelNode(Label())
+			val exitLoop = LabelNode(Label())
+			val setCharArrVal = LabelNode(Label())
+			val throwNull = LabelNode(Label())
+			
+			val tble0 = LabelNode(Label())
+			val tble1 = LabelNode(Label())
+			val tble2 = LabelNode(Label())
+			val tble3 = LabelNode(Label())
+			val tble4 = LabelNode(Label())
+			
+			// First we need to decrypt the method description stored at local var 1
+			// We will turn it into a char array
+			add(VarInsnNode(ALOAD, 1))
+			add(MethodInsnNode(INVOKEVIRTUAL, "java/lang/String", "toCharArray", "()[C", false))
+			add(InsnNode(DUP))
+			// Store it in local var 3
+			add(VarInsnNode(ASTORE, 3))
+			// Find the array length and create our decrypted char array (store in slot 4)
+			add(InsnNode(ARRAYLENGTH))
+			add(IntInsnNode(NEWARRAY, T_CHAR))
+			add(VarInsnNode(ASTORE, 4))
+			
+			// Now loop over our new array
+			add(ldcInt(0))
+			add(VarInsnNode(ISTORE, 5))
+			add(loopStart)
+			add(VarInsnNode(ILOAD, 5)) // index stored at slot 5
+			add(VarInsnNode(ALOAD, 4))
+			add(InsnNode(ARRAYLENGTH))
+			add(JumpInsnNode(IF_ICMPGE, exitLoop)) // If at the end of the loop go to exit
+			
+			
+			add(VarInsnNode(ILOAD, 5))
+			add(ldcInt(5))
+			add(InsnNode(IREM))
+			add(TableSwitchInsnNode(
+				0,
+				4,
+				throwNull,
+				tble0, tble1, tble2, tble3, tble4
+			))
+			
+			
+			add(tble0)
+			add(VarInsnNode(ALOAD, 4)) // Encrypted Char Array
+			add(VarInsnNode(ILOAD, 5)) // index
+			add(InsnNode(CALOAD))
+			add(ldcInt(2))
+			add(InsnNode(IXOR))
+			add(JumpInsnNode(GOTO, setCharArrVal))
+			
+			
+			add(setCharArrVal)
+			add(InsnNode(I2C))
+			add(VarInsnNode(ALOAD, 4)) // Decrypted Char Array
+			add(InsnNode(SWAP))
+			add(VarInsnNode(ILOAD, 5)) // Index
+			add(InsnNode(SWAP))
+			add(InsnNode(CASTORE))
+			// Increment and go to top of loop
+			add(IincInsnNode(5, 1))
+			add(JumpInsnNode(GOTO, loopStart))
+			
+			
+			// If we are here then we have a decrypted char array in slot 4
+			add(exitLoop)
+			
+			
+			
+			add(throwNull)
+			add(InsnNode(ACONST_NULL))
+			add(InsnNode(ATHROW))
+		}
+	}
 	
 	data class MethodCall(val classNode: ClassNode, val methodNode: MethodNode, val insnNode: MethodInsnNode)
 }
