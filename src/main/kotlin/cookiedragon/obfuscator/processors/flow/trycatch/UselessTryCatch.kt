be4658e3eb3f01de03c2cd0e10c2cb38f64fddb9
cookiedragon234 <cookie.dragon234@gmail.com>
2020-01-31 14:10:40 +0000

    Fixed useless try catch

--- src/main/kotlin/cookiedragon/obfuscator/processors/flow/trycatch/UselessTryCatch.kt
+++ src/main/kotlin/cookiedragon/obfuscator/processors/flow/trycatch/UselessTryCatch.kt
@@ -4,10 +4,8 @@ import cookiedragon.obfuscator.CObfuscator
 import cookiedragon.obfuscator.IClassProcessor
 import cookiedragon.obfuscator.kotlin.wrap
 import cookiedragon.obfuscator.utils.InstructionModifier
-import cookiedragon.obfuscator.utils.randomThrowable
 import org.objectweb.asm.Label
-import org.objectweb.asm.Opcodes.ATHROW
-import org.objectweb.asm.Opcodes.GOTO
+import org.objectweb.asm.Opcodes.*
 import org.objectweb.asm.tree.*
 
 /**
@@ -24,34 +22,66 @@ object UselessTryCatch: IClassProcessor {
 				if (CObfuscator.isExcluded(classNode, method))
 					continue
 				
+				var isInitialised = !method.name.startsWith("<")
+				
+				var handler: LabelNode? = null
+				
 				val modifier = InstructionModifier()
-				val encounteredLabels = arrayListOf<LabelNode>()
 				for (insn in method.instructions) {
 					//if (insn is LabelNode) {
 					//	encounteredLabels.add(insn)
 					//}
 					
 					if (insn is MethodInsnNode) {
+						if (insn.opcode == INVOKESPECIAL && !isInitialised) {
+							isInitialised = true
+							continue
+						}
+						
+						if (!isInitialised)
+							continue
+						
+						if (handler == null) {
+							handler = LabelNode(Label())
+							val handler1 = LabelNode(Label())
+							val handlerEnd = LabelNode(Label())
+							
+							val handlerList = InsnList().apply {
+								add(JumpInsnNode(GOTO, handlerEnd))
+								add(handler)
+								add(InsnNode(DUP))
+								add(JumpInsnNode(IFNULL, handler1))
+								add(InsnNode(ATHROW))
+								add(handler1)
+								add(InsnNode(ACONST_NULL))
+								add(InsnNode(ATHROW))
+								add(handlerEnd)
+							}
+							
+							modifier.prepend(method.instructions.first, handlerList)
+						}
+						
 						val beforeLabel = LabelNode(Label())
 						val afterLabel = LabelNode(Label())
-						val handler = LabelNode(Label())
+						val nullThrow = LabelNode(Label())
 						val end = LabelNode(Label())
 						
 						val before = InsnList().apply {
+							//add(nullThrow)
+							//add(InsnNode(ACONST_NULL))
+							//add(InsnNode(ATHROW))
+							//add(JumpInsnNode(GOTO, beforeLabel))
+							//add(handler)
+							//add(JumpInsnNode(IFNULL, handler))
 							add(beforeLabel)
 						}
 						
 						val list = InsnList().apply {
 							add(afterLabel)
-							add(JumpInsnNode(GOTO, end))
-							add(handler)
-							//add(InsnNode(DUP))
-							//add(JumpInsnNode(IFNULL, beforeLabel))
-							add(InsnNode(ATHROW))
-							add(end)
+							//add(end)
 						}
 						
-						method.tryCatchBlocks.add(TryCatchBlockNode(beforeLabel, afterLabel, handler, randomThrowable()))
+						method.tryCatchBlocks.add(TryCatchBlockNode(beforeLabel, afterLabel, handler, null))
 						
 						modifier.prepend(insn, before)
 						modifier.append(insn, list)
