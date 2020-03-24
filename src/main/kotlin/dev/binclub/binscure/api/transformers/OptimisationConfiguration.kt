04f59692adf962f1347802b8d3d5a148e708a6d5
cookiedragon234 <c.o.okiedragon234@gmail.com>
2020-03-24 11:49:37 +0000

    Add enum values optimisation

---
+++ src/main/kotlin/dev/binclub/binscure/api/transformers/OptimisationConfiguration.kt
@@ -0,0 +1,11 @@
+package dev.binclub.binscure.api.transformers
+
+import dev.binclub.binscure.api.TransformerConfiguration
+
+/**
+ * @author cookiedragon234 24/Mar/2020
+ */
+data class OptimisationConfiguration(
+	override val enabled: Boolean = false,
+	val mutableEnumValues: Boolean = false
+): TransformerConfiguration(enabled)
