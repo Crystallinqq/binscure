be93dd2e36aaa6bddfeec9909a8ff1eafd4db574
cookiedragon234 <c.o.okiedragon234@gmail.com>
2020-08-03 14:38:00 +0100

    Rust interop

--- rustinter/src/lib.rs
+++ rustinter/src/lib.rs
@@ -1,20 +1,80 @@
-use std::finally::Finally;
-use jni::JNIEnv;
-use jni::objects::{JClass, JString, JByteArray, JObject};
-use jni::sys::{jstring, jbyteArray};
+#![allow(non_snake_case, non_camel_case_types)]
+
+use std::ffi::c_void;
+use std::ptr::null;
+use jni::{JNIEnv, JavaVM, JNIVersion, NativeMethod};
+use jni::objects::{JClass, ReleaseMode, JObject};
+use jni::sys::{jbyteArray, jint, jclass, JNI_ERR};
+
 use obfstr::obfstr;
+use jni::strings::JNIString;
 
 #[no_mangle]
-pub extern "system" fn Java_dev_binclub_binscure_api_runtime_NativeRuntime_a(
+pub extern fn JNI_OnLoad(
+	vmIn: *mut JavaVM,
+	_reserved: *const c_void
+) -> jint {
+	println!("{}", obfstr!("Binscure Native Library loaded"));
+	
+	let vm =
+		if let Some(vm) = unsafe { vmIn.as_ref() } {
+			vm
+		} else {
+			return JNI_ERR;
+		};
+	
+	let env =
+		if let Ok(env) = vm.get_env() {
+			env
+		} else {
+			return JNI_ERR;
+		};
+	
+	//let env = vm.get_env()
+	//	.expect(obfstr!("Couldn't load JNI Env..."));
+	
+	let class =
+		if let Ok(c) = env.find_class(obfstr!("dev/binclub/binscure/api/runtime/NativeRuntime")) {
+			c
+		} else {
+			return JNI_ERR;
+		};
+	
+	//let class = env.find_class(obfstr!("dev/binclub/binscure/api/runtime/NativeRuntime"))
+	//	.expect(obfstr!("Couldn't find binscure native runtime"));
+	
+	let methods = [
+		NativeMethod {
+			name: JNIString::from(obfstr!("a")),
+			sig: JNIString::from(obfstr!("(Ljava/lang/Object;)Ljava/lang/Object;")),
+			fn_ptr: defineBinscureClass as *mut c_void
+		}
+	];
+	
+	if env.register_native_methods(class, &methods).is_err() {
+		println!("{}", obfstr!("Failed to register binscure native methods"));
+		return JNI_ERR;
+	}
+	
+	return JNIVersion::V6.into();
+}
+
+pub extern "system" fn defineBinscureClass(
 	env: JNIEnv,
 	class: JClass,
 	bytes_in: jbyteArray
-	) {
-	let (bytes, is_copy) = env.get_byte_array_elements(bytes_in).unwrap();
-	(|| {
-		let name = obfstr!("Hi");
-		env.define_class()
-	}).finally(|| {
-		env.release_byte_array_elements(bytes_in, unsafe { bytes.as_mut().unwrap() }, ReleaseMode.NoCopyBack);
-	})
+) -> jclass {
+	let autoBytes = env.get_auto_byte_array_elements(bytes_in, ReleaseMode::NoCopyBack).unwrap();
+	env.get_array_length(bytes_in).expect("");
+	let classBytes: *const jbyte = autoBytes.as_ptr();
+	env.define_class(null(), JObject::null(), autoBytes.as_ptr());
+	jni_non_null_call!(
+            self.internal,
+            DefineClass,
+            name.as_ptr(),
+            loader.into_inner(),
+            buf.as_ptr() as *const jbyte,
+            buf.len() as jsize
+        );
+	return JObject::null().into_inner();
 }
--- src/main/kotlin/dev/binclub/binscure/api/runtime/NativeRuntime.kt
+++ src/main/kotlin/dev/binclub/binscure/api/runtime/NativeRuntime.kt
@@ -38,5 +38,5 @@ object NativeRuntime {
 	 * Load class
 	 * @param a byte array
 	 */
-	external fun a(a: Any)
+	external fun a(a: Any): Any
 }
