e92d7f2a96ff1b82a76f112a6443b890daf39825
mastercooker <c.o.okiedragon234@gmail.com>
2020-09-15 17:35:41 +0100

    Exclusions: match only classes

--- src/main/kotlin/dev/binclub/binscure/configuration/exclusions/PackageBlacklistExcluder.kt
+++ src/main/kotlin/dev/binclub/binscure/configuration/exclusions/PackageBlacklistExcluder.kt
@@ -1,6 +1,5 @@
 package dev.binclub.binscure.configuration.exclusions
 
-import dev.binclub.binscure.utils.originalName
 import org.objectweb.asm.tree.ClassNode
 import org.objectweb.asm.tree.FieldNode
 import org.objectweb.asm.tree.MethodNode
@@ -8,11 +7,16 @@ import org.objectweb.asm.tree.MethodNode
 /**
  * @author cookiedragon234 26/Jan/2020
  */
-class PackageBlacklistExcluder(val aPackage: String): ExclusionConfiguration() {
+class PackageBlacklistExcluder(inPackage: String): ExclusionConfiguration() {
+	private val onlyClasses: Boolean = inPackage.endsWith(';')
+	private val aPackage: String = inPackage.removeSuffix(";")
+	
 	override fun isExcluded(className: String) = className.startsWith(aPackage)
 	override fun isExcluded(classNode: ClassNode) = isExcluded(classNode.originalName ?: classNode.name)
-	override fun isExcluded(parentClass: ClassNode, methodNode: MethodNode) = isExcluded("${parentClass.originalName ?: parentClass.name}.${methodNode.name}")
-	override fun isExcluded(parentClass: ClassNode, fieldNode: FieldNode) = isExcluded("${parentClass.originalName ?: parentClass.name}.${fieldNode.name}")
+	override fun isExcluded(parentClass: ClassNode, methodNode: MethodNode) =
+		!onlyClasses && isExcluded("${parentClass.originalName ?: parentClass.name}.${methodNode.name}")
+	override fun isExcluded(parentClass: ClassNode, fieldNode: FieldNode) =
+		!onlyClasses && isExcluded("${parentClass.originalName ?: parentClass.name}.${fieldNode.name}")
 	
 	override fun toString(): String {
 		return aPackage
