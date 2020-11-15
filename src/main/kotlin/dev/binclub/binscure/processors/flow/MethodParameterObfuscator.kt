55c7c4518e7610647ff8b1b30d8b734bb9de4a52
x4e <x4e_x4e@protonmail.com>
2020-11-15 11:44:07 +0000

    MethodParameterObfuscator format

--- src/main/kotlin/dev/binclub/binscure/processors/flow/MethodParameterObfuscator.kt
+++ src/main/kotlin/dev/binclub/binscure/processors/flow/MethodParameterObfuscator.kt
@@ -14,73 +14,76 @@ import org.objectweb.asm.tree.*
 import java.util.*
 
 
-object MethodParameterObfuscator: IClassProcessor {
-    override val progressDescription: String = "Obfuscating method parameters"
-    override val config = rootConfig.methodParameter
-
-    val methodSecrets: MutableMap<String, Pair<Int, Int>> = HashMap()
-    
-    inline fun mnToStr(cn: ClassNode, mn: MethodNode)  = cn.name + "." + mn.name + mn.desc
-    inline fun mnToStr(insn: MethodInsnNode, add: Boolean = false)  = insn.owner + "." + insn.name + if (!add) insn.desc else insn.desc.replace(")", "I)")
-    inline fun mnToStr(hn: Handle, add: Boolean = false)  = hn.owner + "." + hn.name + if (!add) hn.desc else hn.desc.replace(")", "I)")
-    
-    private fun secretIndex(desc: String): Int {
-        val args = Type.getArgumentTypes(desc)
-        var ourParamIndex = -1 // negate final param append, we need index of it
-        for (arg in args) {
-            ourParamIndex += if (arg.doubleSize) 2 else 1
-        }
-        return ourParamIndex
-    }
-
-    override fun process(classes: MutableCollection<ClassNode>, passThrough: MutableMap<String, ByteArray>) {
-        if (!config.enabled)
-            return
-        
-        if (rootConfig.indirection.enabled) {
-            println("\rWARNING: Method parameter obfuscation cannot be applied on top of indirection")
-            return
-        }
-        
-        forClass(classes) { cn ->
-            if ((cn.access and ACC_ENUM) != 0)
-                return@forClass
-            
-            forMethod(cn) { mn ->
-                if (mn.name[0] == '<' || mn.name == "main")
-                    return@forMethod
-                if ((mn.access and (ACC_NATIVE or ACC_ABSTRACT)) != 0)
-                    return@forMethod
-                if ((mn.access and ACC_STATIC) == 0) // so that we dont have to bother with method inheritance
-                    return@forMethod
-                
-                val newDesc = mn.desc.replace(")", "I)")
-                
-                // if any of the classes other methods occupy the new desc then cancel, we cant do it
-                if (cn.methods.any { it.desc == newDesc })
-                    return@forMethod
-    
-                // take the secret as a parameter
-                mn.desc = newDesc
-    
-    
-                // find the local variable table index that we will insert the final parameter into
-                val ourParamIndex = secretIndex( mn.desc)
-                
-                methodSecrets[mnToStr(cn, mn)] = randomInt() to ourParamIndex
-                
-                // since we've added a new parameter we need to move all local variables up
-                
-                // modify local variable table
-                mn.localVariables?.forEach { lv ->
-                    if (lv.index >= ourParamIndex) {
-                        lv.index += 1
-                    }
-                }
-    
-                // increment local variable references
-                for (insn in mn.instructions) {
-                    when (insn) {
+object MethodParameterObfuscator : IClassProcessor {
+	override val progressDescription: String = "Obfuscating method parameters"
+	override val config = rootConfig.methodParameter
+	
+	val methodSecrets: MutableMap<String, Pair<Int, Int>> = HashMap()
+	
+	inline fun mnToStr(cn: ClassNode, mn: MethodNode) = cn.name + "." + mn.name + mn.desc
+	inline fun mnToStr(insn: MethodInsnNode, add: Boolean = false) =
+		insn.owner + "." + insn.name + if (!add) insn.desc else insn.desc.replace(")", "I)")
+	
+	inline fun mnToStr(hn: Handle, add: Boolean = false) =
+		hn.owner + "." + hn.name + if (!add) hn.desc else hn.desc.replace(")", "I)")
+	
+	private fun secretIndex(desc: String): Int {
+		val args = Type.getArgumentTypes(desc)
+		var ourParamIndex = -1 // negate final param append, we need index of it
+		for (arg in args) {
+			ourParamIndex += if (arg.doubleSize) 2 else 1
+		}
+		return ourParamIndex
+	}
+	
+	override fun process(classes: MutableCollection<ClassNode>, passThrough: MutableMap<String, ByteArray>) {
+		if (!config.enabled)
+			return
+		
+		if (rootConfig.indirection.enabled) {
+			println("\rWARNING: Method parameter obfuscation cannot be applied on top of indirection")
+			return
+		}
+		
+		forClass(classes) { cn ->
+			if ((cn.access and ACC_ENUM) != 0)
+				return@forClass
+			
+			forMethod(cn) { mn ->
+				if (mn.name[0] == '<' || mn.name == "main")
+					return@forMethod
+				if ((mn.access and (ACC_NATIVE or ACC_ABSTRACT)) != 0)
+					return@forMethod
+				if ((mn.access and ACC_STATIC) == 0) // so that we dont have to bother with method inheritance
+					return@forMethod
+				
+				val newDesc = mn.desc.replace(")", "I)")
+				
+				// if any of the classes other methods occupy the new desc then cancel, we cant do it
+				if (cn.methods.any { it.desc == newDesc })
+					return@forMethod
+				
+				// take the secret as a parameter
+				mn.desc = newDesc
+				
+				
+				// find the local variable table index that we will insert the final parameter into
+				val ourParamIndex = secretIndex(mn.desc)
+				
+				methodSecrets[mnToStr(cn, mn)] = randomInt() to ourParamIndex
+				
+				// since we've added a new parameter we need to move all local variables up
+				
+				// modify local variable table
+				mn.localVariables?.forEach { lv ->
+					if (lv.index >= ourParamIndex) {
+						lv.index += 1
+					}
+				}
+				
+				// increment local variable references
+				for (insn in mn.instructions) {
+					when (insn) {
                         is VarInsnNode -> {
                             if (insn.`var` >= ourParamIndex) {
                                 insn.`var` += 1
@@ -91,24 +94,24 @@ object MethodParameterObfuscator: IClassProcessor {
                                 insn.`var` += 1
                             }
                         }
-                    }
-                }
-            }
-        }
-        
-        classes.forEach { cn ->
-            cn.methods.forEach { mn ->
-                // this methods secret
+					}
+				}
+			}
+		}
+		
+		classes.forEach { cn ->
+			cn.methods.forEach { mn ->
+				// this methods secret
                 val (thisSecret, secretIndex) = methodSecrets[mnToStr(cn, mn)].let {
                     (it?.first to it?.second)
                 }
-                
-                // if this method calls any methods with secret parameters we need to pass that parameter to it
-                
-                mn.instructions?.let { list ->
-                    val mod = InstructionModifier()
-                    for (insn in list) {
-                        when (insn) {
+				
+				// if this method calls any methods with secret parameters we need to pass that parameter to it
+				
+				mn.instructions?.let { list ->
+					val mod = InstructionModifier()
+					for (insn in list) {
+						when (insn) {
                             is MethodInsnNode -> {
                                 methodSecrets[mnToStr(insn, true)]?.let { (otherSecret, _) ->
                                     // need to add the parameter
@@ -138,11 +141,11 @@ object MethodParameterObfuscator: IClassProcessor {
                                     }
                                 }
                             }
-                        }
-                    }
-                    mod.apply(list)
-                }
-            }
-        }
-    }
+						}
+					}
+					mod.apply(list)
+				}
+			}
+		}
+	}
 }
